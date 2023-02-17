7c32c3673c2429c11190dda54bfaef58a36ec9f0
Cristian Zavala <czavala.dev@gmail.com>
2023-02-17 10:04:11 -0300

    add set up file for task manager

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const taskRoutes = require('./routes/tasks');
+const connectDB = require('./connect');
+
+
+app.use(express.json());
+
+// Tasks routes
+app.use('/api/v1/tasks', taskRoutes);
+
+
+
+// Wrong URL handler
+app.all('*', (req, res) => {
+    res.status(404).json({ msg: 'Page not found' });
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+// Connecting to DB
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log('Database succesfully connected');
+        app.listen(PORT, () => {
+            console.log(`Server is listening on PORT ${3000}`);
+        })
+    } catch (error) {
+        console.log("Connection failed: " + error);
+    }
+}
+
+start();
+
+
